a6773483bf53cfce3a649d97c132caa7de5a4f11
Rajit Roy <83898887+rajit1995@users.noreply.github.com>
2026-06-11 20:58:03 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Start Ollama in background
+ollama serve &
+
+# Give Ollama a few seconds to boot
+sleep 5
+
+# Pull the model if not already present
+ollama pull llama3.2
+
+# Start your Gradio/LangChain app
+python app.p
