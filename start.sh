8fcfe218f1fbe7ff51aebbcc00040569fb1a4573
Rajit Roy <83898887+rajit1995@users.noreply.github.com>
2026-06-11 22:52:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-# Start Ollama in background
 # Start Ollama bound to all interfaces
 OLLAMA_HOST=0.0.0.0 ollama serve &
 
 # Give Ollama a few seconds to boot
-sleep 5
+sleep 8
 
 # Pull the model if not already present
 ollama pull llama3.2
 
 # Start your Gradio/LangChain app
-python RoyBotgr.py
+python3 RoyBotgr.py
 
